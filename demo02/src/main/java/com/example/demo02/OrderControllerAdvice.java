caf85bdabfbda11cf02272e97e3d71651cb92f78
up1 <somkiat.p@gmail.com>
2025-09-23 12:33:13 +0700

    Add actuator and prometheus

--- demo02/src/main/java/com/example/demo02/OrderControllerAdvice.java
+++ demo02/src/main/java/com/example/demo02/OrderControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.example.demo02;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,15 +15,17 @@ public class OrderControllerAdvice {
     public ResponseEntity<ErrorMessageResponse> badRequest(Exception ex){
         ErrorMessageResponse myException = new ErrorMessageResponse("Bad request with "+ ex.getMessage());
         ResponseEntity<ErrorMessageResponse> response =
-                new ResponseEntity<>(myException, HttpStatusCode.valueOf(400));
+                new ResponseEntity<>(myException, HttpStatus.NOT_FOUND);
         return response;
     }
+
+    private final static int ORDER_NOT_FOUND = 404;
 
     @ExceptionHandler(exception = OrderNotFoundException.class)
     public ResponseEntity<ErrorMessageResponse> orderNotFound(Exception ex){
         ErrorMessageResponse myException = new ErrorMessageResponse(ex.getMessage());
         ResponseEntity<ErrorMessageResponse> response =
-                new ResponseEntity<>(myException, HttpStatusCode.valueOf(404));
+                new ResponseEntity<>(myException, HttpStatusCode.valueOf(ORDER_NOT_FOUND));
         return response;
     }
 
