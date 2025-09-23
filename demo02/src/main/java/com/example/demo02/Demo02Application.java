f668950ae67fe1c1f7bd898cf54add2632f02f6d
up1 <somkiat.p@gmail.com>
2025-09-23 10:25:06 +0700

    List of all beans

--- demo02/src/main/java/com/example/demo02/Demo02Application.java
+++ demo02/src/main/java/com/example/demo02/Demo02Application.java
@@ -2,12 +2,19 @@ package com.example.demo02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Demo02Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo02Application.class, args);
-	}
+		ConfigurableApplicationContext context
+                = SpringApplication.run(Demo02Application.class, args);
+        String[] beans = context.getBeanDefinitionNames();
+        for (String bean : beans) {
+            System.out.println(bean);
+        }
+        System.out.println(context.getBeanDefinitionCount());
+    }
 
 }
